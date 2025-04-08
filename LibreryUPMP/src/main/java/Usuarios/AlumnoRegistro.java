9c4cd6498ee7c806ef440cdeffca7b2ba1cf73d0
DARO <daniel29072001@gmsil.com>
2025-04-08 14:27:24 -0600

    Carga 08-04-2025 TARDE

--- LibreryUPMP/src/main/java/Usuarios/AlumnoRegistro.java
+++ LibreryUPMP/src/main/java/Usuarios/AlumnoRegistro.java
@@ -19,68 +19,143 @@ public class AlumnoRegistro extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        LOGO = new javax.swing.JLabel();
+        REGRESAR = new javax.swing.JToggleButton();
+        VISITA = new javax.swing.JToggleButton();
+        REGISTRAR_NUEVO = new javax.swing.JToggleButton();
+        PRESTAMO_REG1 = new javax.swing.JToggleButton();
+        USO_COMPUTADORA = new javax.swing.JToggleButton();
         jPanel4 = new javax.swing.JPanel();
-        jToggleButton1 = new javax.swing.JToggleButton();
-        jToggleButton2 = new javax.swing.JToggleButton();
-        jToggleButton3 = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Registro de Alumnos");
         setBackground(new java.awt.Color(32, 59, 104));
-        setMaximumSize(new java.awt.Dimension(740, 500));
-        setMinimumSize(new java.awt.Dimension(740, 500));
+        setLocation(new java.awt.Point(0, 0));
+        setMaximumSize(new java.awt.Dimension(1250, 700));
+        setMinimumSize(new java.awt.Dimension(1250, 700));
+        setPreferredSize(new java.awt.Dimension(1250, 700));
+        setSize(new java.awt.Dimension(1250, 700));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(32, 59, 104));
-        jPanel1.setMaximumSize(new java.awt.Dimension(740, 500));
-        jPanel1.setMinimumSize(new java.awt.Dimension(740, 500));
+        jPanel1.setMaximumSize(new java.awt.Dimension(1250, 700));
+        jPanel1.setMinimumSize(new java.awt.Dimension(1250, 700));
+        jPanel1.setName(""); // NOI18N
+        jPanel1.setPreferredSize(new java.awt.Dimension(1250, 700));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel1.setFont(new java.awt.Font("Gotham", 0, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("REGISTROS");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 20, 740, 30));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 30, 740, 30));
+
+        LOGO.setIcon(new javax.swing.ImageIcon(getClass().getResource("/LOGO M c-letra LAMETRO.png"))); // NOI18N
+        jPanel1.add(LOGO, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 20, -1, -1));
+
+        REGRESAR.setFont(new java.awt.Font("Gotham", 0, 18)); // NOI18N
+        REGRESAR.setText("REGRESAR");
+        REGRESAR.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                REGRESARActionPerformed(evt);
+            }
+        });
+        jPanel1.add(REGRESAR, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 560, 150, 60));
+
+        VISITA.setFont(new java.awt.Font("Gotham", 0, 18)); // NOI18N
+        VISITA.setText("VISITA");
+        VISITA.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                VISITAActionPerformed(evt);
+            }
+        });
+        jPanel1.add(VISITA, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, 175, 75));
+
+        REGISTRAR_NUEVO.setFont(new java.awt.Font("Gotham", 0, 18)); // NOI18N
+        REGISTRAR_NUEVO.setText("REGISTRATE");
+        REGISTRAR_NUEVO.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                REGISTRAR_NUEVOActionPerformed(evt);
+            }
+        });
+        jPanel1.add(REGISTRAR_NUEVO, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 380, 175, 75));
+
+        PRESTAMO_REG1.setFont(new java.awt.Font("Gotham", 0, 18)); // NOI18N
+        PRESTAMO_REG1.setText("PRESTAMO");
+        PRESTAMO_REG1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                PRESTAMO_REG1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(PRESTAMO_REG1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 220, 175, 75));
+
+        USO_COMPUTADORA.setFont(new java.awt.Font("Gotham", 0, 18)); // NOI18N
+        USO_COMPUTADORA.setText("<html><center>USOO DE COMPUTADORA<html>");
+        USO_COMPUTADORA.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                USO_COMPUTADORAActionPerformed(evt);
+            }
+        });
+        jPanel1.add(USO_COMPUTADORA, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 300, 175, 75));
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+            .addGap(0, 980, Short.MAX_VALUE)
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 370, Short.MAX_VALUE)
+            .addGap(0, 530, Short.MAX_VALUE)
         );
 
-        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 720, 370));
+        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 90, 980, 530));
 
-        jToggleButton1.setText("REGISTRO");
-        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jToggleButton1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jToggleButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 80, -1, -1));
-
-        jToggleButton2.setText("VISITA");
-        jPanel1.add(jToggleButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, -1, -1));
-
-        jToggleButton3.setText("PRESTAMO");
-        jPanel1.add(jToggleButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 80, -1, -1));
-
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 740, 500));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1250, 700));
 
         getAccessibleContext().setAccessibleDescription("Registrar la visita, prestamo de libros o el uso de las computadoras de la Biblioteca");
 
         pack();
-        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jToggleButton1ActionPerformed
+    private void VISITAActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_VISITAActionPerformed
+        // PONER FORMULARIO DE VISITA
+        
+        
+        
+    }//GEN-LAST:event_VISITAActionPerformed
 
+    private void PRESTAMO_REG1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PRESTAMO_REG1ActionPerformed
+        // PONER FORMULARIO DE PRESTAMO
+        
+        
+        
+    }//GEN-LAST:event_PRESTAMO_REG1ActionPerformed
+
+    private void USO_COMPUTADORAActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_USO_COMPUTADORAActionPerformed
+        // PONER USO DE LA COMPUTADORA
+        
+        
+        
+    }//GEN-LAST:event_USO_COMPUTADORAActionPerformed
+
+    private void REGISTRAR_NUEVOActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_REGISTRAR_NUEVOActionPerformed
+        // FORMULARIO SI EL ALUMNO ES NUEVO
+        
+        
+        
+    }//GEN-LAST:event_REGISTRAR_NUEVOActionPerformed
+
+    private void REGRESARActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_REGRESARActionPerformed
+        // REGRESAR A LA PANTALLA ANTERIOR - POR CUALQUIER CAUSA 
+        
+        
+        
+    }//GEN-LAST:event_REGRESARActionPerformed
+
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -118,11 +193,14 @@ public class AlumnoRegistro extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel LOGO;
+    private javax.swing.JToggleButton PRESTAMO_REG1;
+    private javax.swing.JToggleButton REGISTRAR_NUEVO;
+    private javax.swing.JToggleButton REGRESAR;
+    private javax.swing.JToggleButton USO_COMPUTADORA;
+    private javax.swing.JToggleButton VISITA;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel4;
-    private javax.swing.JToggleButton jToggleButton1;
-    private javax.swing.JToggleButton jToggleButton2;
-    private javax.swing.JToggleButton jToggleButton3;
     // End of variables declaration//GEN-END:variables
 }
